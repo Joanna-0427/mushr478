c9889c520ad930d85c8c212de0edf8b63f7d5fdb
Joanna0427 <yuting.wan0427@gmail.com>
2026-09-12 19:22:07 -0700

    lab1

--- introduction/src/introduction/fibonacci.py
+++ introduction/src/introduction/fibonacci.py
@@ -13,5 +13,12 @@ def compute_fibonacci(n):
     3
     """
     # BEGIN QUESTION 1.1
-    "*** REPLACE THIS LINE ***"
+    if n == 0:
+        return 0
+    if n == 1:
+        return 1
+    a, b = 0, 1
+    for _ in range(2,n+1):
+        a, b = b, a+b
+    return b
     # END QUESTION 1.1
